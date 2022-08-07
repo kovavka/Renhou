b6a9542f2f39f5352956bed4a0b25c190e0a92ff
Gin Kapger <gikapger@gmail.com>
2022-08-07 16:45:21 +0500

    init

---
+++ src/web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button} from "@common/react/components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
