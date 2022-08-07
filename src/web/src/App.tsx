1ab6998bccdd11ca259fcda0d1ab258c0caa0560
Gin Kapger <gikapger@gmail.com>
2022-08-07 17:00:55 +0500

    change button import

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Button} from "@common/react/components/Button";
+import {Button} from "../../common/react/components/Button";
 
 function App() {
   return (
