681973421a7ab8d03b4e8c7da5f14fa483a9f836
Gin Kapger <gikapger@gmail.com>
2022-08-11 07:20:10 +0500

    check tempai

---
+++ src/common/canvas/utils/tile/isTempai.test.ts
@@ -0,0 +1,226 @@
+import {isTempai} from "./isTempai";
+import {Tile} from "../../core/game-types/Tile";
+import {SuitType} from "../../core/game-types/SuitType";
+
+const man1 =  {type: SuitType.MANZU, value: 1}
+const man2 =  {type: SuitType.MANZU, value: 2}
+const man3 =  {type: SuitType.MANZU, value: 3}
+const man4 =  {type: SuitType.MANZU, value: 4}
+const man5 =  {type: SuitType.MANZU, value: 5}
+const man6 =  {type: SuitType.MANZU, value: 6}
+const man7 =  {type: SuitType.MANZU, value: 7}
+const man8 =  {type: SuitType.MANZU, value: 8}
+const man9 =  {type: SuitType.MANZU, value: 9}
+const pin1 =  {type: SuitType.PINZU, value: 1}
+const pin2 =  {type: SuitType.PINZU, value: 2}
+const pin3 =  {type: SuitType.PINZU, value: 3}
+const pin9 =  {type: SuitType.PINZU, value: 9}
+const sou1 =  {type: SuitType.SOUZU, value: 1}
+const sou2 =  {type: SuitType.SOUZU, value: 2}
+const sou3 =  {type: SuitType.SOUZU, value: 3}
+const sou9 =  {type: SuitType.SOUZU, value: 9}
+const haku =  {type: SuitType.JIHAI, value: 1}
+const hatsu =  {type: SuitType.JIHAI, value: 2}
+const chun =  {type: SuitType.JIHAI, value: 3}
+const ton =  {type: SuitType.JIHAI, value: 4}
+const nan =  {type: SuitType.JIHAI, value: 5}
+const shaa =  {type: SuitType.JIHAI, value: 6}
+const pei =  {type: SuitType.JIHAI, value: 7}
+
+describe('isTempai', () => {
+    describe('Chiitoi', () => {
+        it('Tempai when there are 6 pairs and a unique tile', () => {
+            const tiles = [man1, man1, man9, man9, pin1, pin1, pin9, pin9, sou1, sou1, sou9, sou9, haku]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 6 pairs and a unique tile', () => {
+            const tiles = [man1, man1, man9, man9, pin1, pin1, pin9, pin9, sou1, sou1, sou9, sou9, haku]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Not a templai when there are 6 pairs and 1 not a unique tile', () => {
+            const tiles = [man1, man1, man9, man9, pin1, pin1, pin9, pin9, sou1, sou1, sou9, sou9, sou9]
+            expect(isTempai(tiles)).toBe(false)
+        })
+        it('Not a templai when there are 4 pairs, 4 duplicate tiles and a unique tile', () => {
+            const tiles = [man1, man1, man9, man9, pin1, pin1, pin9, pin9, sou1, sou1, sou1, sou1, sou9]
+            expect(isTempai(tiles)).toBe(false)
+        })
+    })
+
+    describe('Kokushi muso', () => {
+        it('Tempai when there are 12 single terminal and honor tiles + pair for one of them', () => {
+            const tiles = [man1, man9, pin1, pin9, sou1, sou9, haku, hatsu, chun, ton, nan, shaa, shaa]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 13 single terminal and honor tiles', () => {
+            const tiles = [man1, man9, pin1, pin9, sou1, sou9, haku, hatsu, chun, ton, nan, shaa, pei]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Not a templai when there is at least 1 man/oin/sou tile from 2 to 8', () => {
+            const tiles = [man1, man9, pin1, pin9, sou1, sou9, haku, hatsu, chun, ton, nan, shaa, man2]
+            expect(isTempai(tiles)).toBe(false)
+        })
+    })
+
+    describe('Simple hand structure', () => {
+        it('Tempai when there are 4 sequential melds and 1 tile', () => {
+            const tiles = [man1, man2, man3, man7, man8, man9, pin1, pin2, pin3, sou1, sou2, sou3, hatsu]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 4 identical melds and 1 tile', () => {
+            const tiles = [man1, man1, man1, man7, man7, man7, pin1, pin1, pin1, sou2, sou2, sou2, sou9]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 4 melds and 1 tile', () => {
+            const tiles = [man1, man1, man1, man7, man8, man9, pin1, pin1, pin1, sou2, sou2, sou2, sou9]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 3 melds, 1 pair and 1 ryanmen', () => {
+            const tiles = [man1, man1, man1, pin1, pin1, pin1, sou2, sou2, sou2, sou9, sou9, man2, man3]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 3 melds, 1 pair and 1 penchan', () => {
+            const tiles = [pin1, pin1, pin1, pin2, pin2, pin2, sou2, sou2, sou2, sou9, sou9, man1, man2]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 3 melds, 1 pair and 1 kanchan', () => {
+            const tiles = [pin1, pin1, pin1, pin2, pin2, pin2, sou2, sou2, sou2, sou9, sou9, man1, man3]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 3 melds and 2 pairs', () => {
+            const tiles = [pin1, pin1, pin1, pin2, pin2, pin2, sou2, sou2, sou2, sou9, sou9, man1, man1]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Not a templai when there are 3 melds and 2 ryanmens', () => {
+            const tiles = [pin1, pin1, pin1, pin2, pin2, pin2, sou2, sou2, sou2, man2, man3, man7, man8]
+            expect(isTempai(tiles)).toBe(false)
+        })
+        it('Not a templai when there are 3 melds, 1 pair and 2 separated tiles', () => {
+            const tiles = [pin1, pin1, pin1, pin2, pin2, pin2, sou2, sou2, sou2, man2, man2, haku, hatsu]
+            expect(isTempai(tiles)).toBe(false)
+        })
+    })
+
+    describe('Complicated hand structure in a one suit', () => {
+        it('Tempai when there are 4 melds and 1 tile', () => {
+            const meld1 = [man1, man2, man3]
+            const meld2 = [man2, man3, man4]
+            const meld3 = [man5, man6, man7]
+            const meld4 = [man5, man5, man5]
+            const tiles = [...meld1, ...meld2, ...meld3, ...meld4, man9]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 3 melds, pair and any wait', () => {
+            const meld1 = [man1, man2, man3]
+            const meld2 = [man2, man3, man4]
+            const meld3 = [man5, man6, man7]
+            const pair = [man5, man5]
+            const wait = [man7, man8]
+            // could be also interpreted as not a tepmai: 123 234 555 67 78
+            const tiles = [...meld1, ...meld2, ...meld3, ...pair, ...wait]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Not a templai when there are 3 melds, 1 pair and 2 separated tiles', () => {
+            const meld1 = [man1, man1, man1]
+            const meld2 = [man1, man2, man3]
+            const meld3 = [man2, man3, man4]
+            const pair = [man7, man7]
+            const separatedTiles = [man4, man9]
+            const tiles = [...meld1, ...meld2, ...meld3, ...pair, ...separatedTiles]
+            expect(isTempai(tiles)).toBe(false)
+        })
+    })
+
+    describe('1 tile', () => {
+        it('Always tempai', () => {
+            expect(isTempai([man1])).toBe(false)
+        })
+    })
+
+    describe('4 tiles', () => {
+        it('Tempai when there are 1 meld and 1 tile', () => {
+            const tiles = [man1, man2, man3, hatsu]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 2 pairs', () => {
+            const tiles = [man1, man1, man3, man3]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Not a templai when there are 1 pair and 2 separated tiles', () => {
+            const tiles = [man1, man1, man3, hatsu]
+            expect(isTempai(tiles)).toBe(false)
+        })
+    })
+
+    describe('7 tiles', () => {
+        it('Tempai when there are 2 melds and 1 tile', () => {
+            const tiles = [man1, man2, man3, sou1, sou1, sou1, hatsu]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 1 meld and 2 pairs', () => {
+            const tiles = [sou1, sou1, sou1, man1, man1, man3, man3]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Not a templai when there are 1 meld, 1 pair and 2 separated tiles', () => {
+            const tiles = [sou1, sou1, sou1, man1, man1, man3, hatsu]
+            expect(isTempai(tiles)).toBe(false)
+        })
+    })
+
+    describe('9 tiles', () => {
+        it('Tempai when there are 3 melds and 1 tile', () => {
+            const tiles = [man1, man2, man3, sou1, sou1, sou1, pin2, pin2, pin2, hatsu]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Tempai when there are 2 melds and 2 pairs', () => {
+            const tiles = [sou1, sou1, sou1, pin1, pin2, pin3, man1, man1, man3, man3]
+            expect(isTempai(tiles)).toBe(true)
+        })
+        it('Not a templai when there are 2 melds, 1 pair and 2 separated tiles', () => {
+            const tiles = [sou1, sou1, sou1, pin1, pin2, pin3, man1, man1, man3, hatsu]
+            expect(isTempai(tiles)).toBe(false)
+        })
+    })
+
+    describe('Impossible hand length', () => {
+        it('Not a templai when hand is empty', () => {
+            expect(isTempai([])).toBe(false)
+        })
+        it('Not a templai when tiles length > 13', () => {
+            const tiles: Tile[] = []
+            for (let i = 0; i < 15; i++) {
+                tiles.push({type: SuitType.MANZU, value: 1})
+            }
+            expect(isTempai(tiles)).toBe(false)
+        })
+        it('Not a templai when tiles length > 1 && < 4', () => {
+            const tiles: Tile[] = []
+            for (let i = 0; i < 4; i++) {
+                tiles.push({type: SuitType.MANZU, value: 1})
+            }
+            expect(isTempai(tiles)).toBe(false)
+        })
+        it('Not a templai when tiles length > 1 && < 7', () => {
+            const tiles: Tile[] = []
+            for (let i = 0; i < 7; i++) {
+                tiles.push({type: SuitType.MANZU, value: 1})
+            }
+            expect(isTempai(tiles)).toBe(false)
+        })
+        it('Not a templai when tiles length > 4 && < 10', () => {
+            const tiles: Tile[] = []
+            for (let i = 0; i < 10; i++) {
+                tiles.push({type: SuitType.MANZU, value: 1})
+            }
+            expect(isTempai(tiles)).toBe(false)
+        })
+        it('Not a templai when tiles length > 10 && < 13', () => {
+            const tiles: Tile[] = []
+            for (let i = 0; i < 13; i++) {
+                tiles.push({type: SuitType.MANZU, value: 1})
+            }
+            expect(isTempai(tiles)).toBe(false)
+        })
+    })
+
+})
---
+++ src/common/canvas/utils/tile/isTempai.ts
@@ -0,0 +1,534 @@
+import {Tile} from "../../core/game-types/Tile";
+import {SuitType} from "../../core/game-types/SuitType";
+
+enum WaitPatternType {
+    TANKI,
+    SHANPON,
+    KANCHAN,
+    RYANMEN_PENCHAN,
+}
+
+interface WaitPattern {
+    tiles: number[]
+    type: WaitPatternType
+}
+
+interface SuitStructure {
+    sets: number[][]
+    unusedTiles: number[]
+    waitPatterns: WaitPattern[]
+    pair: number | undefined
+    remainingTiles: number[] // todo move outside
+}
+
+interface HandStructure {
+    manTiles: number[]
+    pinTiles: number[]
+    souTiles: number[]
+    honorTiles: number[]
+}
+
+interface HandWaitStructure {
+    man?: SuitStructure
+    pin?: SuitStructure
+    sou?: SuitStructure
+    honors?: SuitStructure
+}
+
+export function isTempai(tiles: Tile[]): boolean {
+    if (tiles.length === 0 || tiles.length > 13 || (tiles.length > 1 && tiles.length < 4) &&) {
+        return false
+    }
+
+    if (isChiitoiTempai(tiles) || isKokushiMusoTempai(tiles)) {
+        return true
+    }
+
+    const hand = getHand(tiles)
+    const {manTiles, pinTiles, souTiles, honorTiles} = hand
+
+    const manSuits = processSuit([], getSimpleSuitStructure(manTiles))
+    const pinSuits = processSuit([], getSimpleSuitStructure(pinTiles))
+    const souSuits = processSuit([], getSimpleSuitStructure(souTiles))
+    const honors = processSuit([], getSimpleSuitStructure(honorTiles))
+
+    const possibleManSuits = getPossibleStructures(manSuits)
+    const possiblePinSuits = getPossibleStructures(pinSuits)
+    const possibleSouSuits = getPossibleStructures(souSuits)
+    const possibleHonors = getPossibleStructures(honors)
+
+
+    let handWaitStructures: HandWaitStructure[] =
+        processPossibleManSuits(possibleManSuits, possiblePinSuits, possibleSouSuits, possibleHonors)
+
+    for (let handWaitStructure of handWaitStructures) {
+        if (isReadyHand(handWaitStructure)) {
+            return true
+        }
+    }
+
+    return false
+}
+
+function isReadyHand(hand: HandWaitStructure) {
+    let pairsCount = 0
+    let waits: WaitPattern[] = []
+    if (hand.man) {
+        if (hand.man.pair) {
+            pairsCount++
+        }
+        waits.push(...hand.man.waitPatterns)
+    }
+    if (hand.pin) {
+        if (hand.pin.pair) {
+            pairsCount++
+        }
+        waits.push(...hand.pin.waitPatterns)
+    }
+    if (hand.sou) {
+        if (hand.sou.pair) {
+            pairsCount++
+        }
+        waits.push(...hand.sou.waitPatterns)
+    }
+    if (hand.honors) {
+        if (hand.honors.pair) {
+            pairsCount++
+        }
+        waits.push(...hand.honors.waitPatterns)
+    }
+
+    if (pairsCount > 2 || waits.length > 2) {
+        return false
+    }
+
+    if (pairsCount === 2 && waits.length === 0) {
+        return true //SHANPON
+    }
+
+    if (
+        pairsCount === 0 &&
+        ((waits.length === 1 && waits[0].type === WaitPatternType.TANKI) ||
+            (waits.length === 2 && waits.every(pattern => pattern.type === WaitPatternType.SHANPON)))
+    ) {
+        return true
+    }
+
+    if (
+        pairsCount === 1 &&
+        waits.length === 1 &&
+        [WaitPatternType.KANCHAN, WaitPatternType.RYANMEN_PENCHAN].includes(waits[0].type)
+    ) {
+        return true
+    }
+
+    return false
+}
+
+function processPossibleManSuits(
+    possibleManSuits: SuitStructure[],
+    possiblePinSuits: SuitStructure[],
+    possibleSouSuits: SuitStructure[],
+    possibleHonors: SuitStructure[]
+) {
+    if (!possibleManSuits.length) {
+        return processPossiblePinSuits(undefined, possiblePinSuits, possibleSouSuits, possibleHonors)
+    }
+
+    let handPatterns: HandWaitStructure[] = []
+    for (let manStructure of possibleManSuits) {
+        handPatterns.push(
+            ...processPossiblePinSuits(manStructure, possiblePinSuits, possibleSouSuits, possibleHonors)
+        )
+    }
+    return handPatterns
+}
+
+function processPossibleSouSuits(
+    manStructure: SuitStructure | undefined,
+    pinStructure: SuitStructure | undefined,
+    possibleSouSuits: SuitStructure[],
+    possibleHonors: SuitStructure[]
+) {
+    if (!possibleSouSuits.length) {
+        return processPossibleHonors(manStructure, pinStructure, undefined, possibleHonors)
+    }
+
+    let handPatterns: HandWaitStructure[] = []
+    for (let souStructure of possibleSouSuits) {
+        handPatterns.push(...processPossibleHonors(manStructure, pinStructure, souStructure, possibleHonors))
+    }
+    return handPatterns
+}
+
+function processPossiblePinSuits(
+    manStructure: SuitStructure | undefined,
+    possiblePinSuits: SuitStructure[],
+    possibleSouSuits: SuitStructure[],
+    possibleHonors: SuitStructure[]
+) {
+    if (!possiblePinSuits.length) {
+        return processPossibleSouSuits(manStructure, undefined, possibleSouSuits, possibleHonors)
+    }
+
+    let handPatterns: HandWaitStructure[] = []
+    for (let pinStructure of possiblePinSuits) {
+        handPatterns.push(...processPossibleSouSuits(manStructure, pinStructure, possibleSouSuits, possibleHonors))
+    }
+    return handPatterns
+}
+
+function processPossibleHonors(
+    manStructure: SuitStructure | undefined,
+    pinStructure: SuitStructure | undefined,
+    souStructure: SuitStructure | undefined,
+    possibleHonors: SuitStructure[]
+) {
+    if (!possibleHonors.length) {
+        return [
+            <HandWaitStructure>{
+                man: manStructure,
+                pin: pinStructure,
+                sou: souStructure,
+            },
+        ]
+    }
+
+    let handPatterns: HandWaitStructure[] = []
+    for (let honorStructure of possibleHonors) {
+        handPatterns.push(<HandWaitStructure>{
+            man: manStructure,
+            pin: pinStructure,
+            sou: souStructure,
+            honors: honorStructure,
+        })
+    }
+
+    return handPatterns
+}
+
+function getHand(tiles: Tile[]): HandStructure {
+    const manTiles: number[] = []
+    const pinTiles: number[] = []
+    const souTiles: number[] = []
+    const honorTiles: number[] = []
+
+    tiles.forEach(tile => {
+        switch (tile.type) {
+            case SuitType.MANZU:
+                manTiles.push(tile.value)
+                break
+            case SuitType.PINZU:
+                pinTiles.push(tile.value)
+                break
+            case SuitType.SOUZU:
+                souTiles.push(tile.value)
+                break
+            case SuitType.JIHAI:
+                honorTiles.push(tile.value)
+                break
+        }
+    })
+
+    return {
+        manTiles,
+        pinTiles,
+        souTiles,
+        honorTiles,
+    }
+}
+
+function isChiitoiTempai(tiles: Tile[]): boolean {
+    if (tiles.length !== 13) {
+        return false
+    }
+
+    return groupDuplicates(tiles).length === 7
+}
+
+function isKokushiMusoTempai(tiles: Tile[]): boolean {
+    if (tiles.length !== 13) {
+        return false
+    }
+
+    if (tiles.some(tile => tile.value > 1 && tile.value < 9 && tile.type !== SuitType.JIHAI)) {
+        return false
+    }
+
+
+    const duplicateGroups = groupDuplicates(tiles)
+    // it's either 12 single tiles + pair for one of them or 13 single tiles
+    return duplicateGroups.length > 11
+}
+
+function getSimpleSuitStructure(tiles: number[]): SuitStructure {
+    return <SuitStructure>{
+        sets: [],
+        unusedTiles: [],
+        waitPatterns: [],
+        pair: undefined,
+        remainingTiles: tiles,
+    }
+}
+
+function getPossibleStructures(structures: SuitStructure[] | undefined): SuitStructure[] {
+    if (!structures || !structures.length) {
+        return []
+    }
+
+    return structures.filter(structure => isPossibleWaitPatterns(structure.waitPatterns))
+}
+
+function isPossibleWaitPatterns(patterns: WaitPattern[]): boolean {
+    if (!patterns.length) {
+        return true
+    }
+    if (patterns.length > 2) {
+        return false
+    }
+    if (
+        patterns.length === 1 &&
+        [WaitPatternType.TANKI, WaitPatternType.KANCHAN, WaitPatternType.RYANMEN_PENCHAN].includes(patterns[0].type)
+    ) {
+        return true
+    }
+    if (patterns.length === 2 && patterns.every(pattern => pattern.type === WaitPatternType.SHANPON)) {
+        return true
+    }
+    return false
+}
+
+function processSuit(allVariations: SuitStructure[], structure: SuitStructure, isHonors: boolean = false): SuitStructure[] {
+    if (structure.remainingTiles.length < 3) {
+        structure.unusedTiles = structure.unusedTiles.concat(structure.remainingTiles)
+        structure.remainingTiles = []
+        // return [structure]
+        trySetStructure(allVariations, structure, isHonors)
+        return allVariations
+    }
+
+    let unusedTiles = structure.unusedTiles.slice(0)
+    let remainingHand = structure.remainingTiles.slice(0)
+
+    // let childStructures: HandStructure[] = []
+    for (let tile of structure.remainingTiles) {
+        let sets = getSets(tile, remainingHand)
+        for (let set of sets) {
+            let newStructure = <SuitStructure>{
+                sets: structure.sets.length ? structure.sets.concat([set]) : [set],
+                remainingTiles: nextTiles(remainingHand, ...set),
+                unusedTiles: unusedTiles.slice(0),
+            }
+            processSuit(allVariations, newStructure, isHonors)
+        }
+        unusedTiles.push(tile)
+        remainingHand = nextTiles(remainingHand, tile)
+    }
+
+    let parentStructure = <SuitStructure>{
+        sets: structure.sets,
+        remainingTiles: remainingHand,
+        unusedTiles: unusedTiles,
+    }
+    trySetStructure(allVariations, parentStructure, isHonors)
+
+    return allVariations
+}
+
+function trySetStructure(allVariations: SuitStructure[], structure: SuitStructure, isHonors: boolean) {
+    let possibleStructures = allVariations.filter(
+        x =>
+            x.sets.length === structure.sets.length &&
+            x.unusedTiles.join('') === structure.unusedTiles.join('') &&
+            x.sets.map(n => n.join('')).join(' ') === structure.sets.map(n => n.join('')).join(' ')
+    )
+
+    if (!possibleStructures.length) {
+        let data = getPairsAndWaitings(structure.unusedTiles, isHonors)
+        structure.pair = data.pair
+        structure.waitPatterns = data.waitPatterns
+        allVariations.push(structure)
+    }
+}
+
+function getPairsAndWaitings(
+    unusedTiles: number[],
+    isHonors: boolean
+): {pair: number | undefined; waitPatterns: WaitPattern[]} {
+    let allPairs = getPairs(unusedTiles, true)
+
+    let availablePair: number | undefined
+    let remainingTiles = unusedTiles.slice(0)
+
+    //if wait pattern is shanpon or hand has too mush pairs or pair and other tiles -> there is no pair, it's waitings
+    if (allPairs.length === 1) {
+        availablePair = allPairs[0]
+        let pairTile = allPairs[0]
+        remainingTiles = nextTiles(remainingTiles, pairTile, pairTile)
+    }
+    if (!remainingTiles.length) {
+        return {pair: availablePair, waitPatterns: []}
+    }
+
+    let waitPatterns: WaitPattern[] = []
+    while (remainingTiles.length) {
+        let waitPattern = getWaitPatternFrom(remainingTiles[0], remainingTiles, isHonors)
+        waitPatterns.push(waitPattern)
+        remainingTiles = nextTiles(remainingTiles, ...waitPattern.tiles)
+    }
+
+    //it's impossible hand contains pair and tanki wait
+    if (waitPatterns.find(x => x.type === WaitPatternType.TANKI)) {
+        availablePair = undefined
+        waitPatterns = []
+        remainingTiles = unusedTiles.slice(0)
+        while (remainingTiles.length) {
+            let waitPattern = getWaitPatternFrom(remainingTiles[0], remainingTiles, isHonors)
+            waitPatterns.push(waitPattern)
+            remainingTiles = nextTiles(remainingTiles, ...waitPattern.tiles)
+        }
+    }
+
+    return {pair: availablePair, waitPatterns: waitPatterns}
+}
+
+function nextTiles(hand: number[], ...tiles: number[]): number[] {
+    let str = hand.join('')
+    for (let tile of tiles) {
+        str = str.replace(tile.toString(), '')
+    }
+    return str.split('').map(x => Number(x))
+}
+
+function getSets(tile: number, str: number[]): number[][] {
+    let sets: number[][] = []
+    let chii = getChii(tile, str)
+    if (chii) {
+        sets.push(chii)
+    }
+
+    let pon = getPon(tile, str)
+    if (pon) {
+        sets.push(pon)
+    }
+    return sets
+}
+
+function getChii(tile: number, handPart: number[]): number[] | undefined {
+    if (tile >= 8) {
+        return undefined
+    }
+
+    let next1 = tile + 1
+    let next2 = tile + 2
+    if (hasUniqueTiles(handPart, tile, next1, next2)) {
+        return [tile, next1, next2]
+    }
+
+    return undefined
+}
+
+function getPon(tile: number, handPart: number[]): number[] | undefined {
+    if (hasDuplicateTiles(handPart, tile, 3)) {
+        return [tile, tile, tile]
+    }
+    return undefined
+}
+
+function getWaitPatternFrom(tile: number, handPart: number[], isHonors: boolean): WaitPattern {
+    if (hasDuplicateTiles(handPart, tile, 2)) {
+        return <WaitPattern>{
+            tiles: [tile, tile],
+            type: WaitPatternType.SHANPON,
+        }
+    }
+
+    if (tile === 9 || isHonors) {
+        //not shanpon => only tanki
+        return <WaitPattern>{
+            tiles: [tile],
+            type: WaitPatternType.TANKI,
+        }
+    }
+
+    let next1 = tile + 1
+    if (hasUniqueTiles(handPart, tile, next1)) {
+        return <WaitPattern>{
+            tiles: [tile, next1],
+            type: WaitPatternType.RYANMEN_PENCHAN,
+        }
+    }
+
+    if (tile === 8) {
+        //not shanpon, ryanmen or penchan => only tanki
+        return <WaitPattern>{
+            tiles: [tile],
+            type: WaitPatternType.TANKI,
+        }
+    }
+
+    let next2 = tile + 2
+    if (hasUniqueTiles(handPart, tile, next2)) {
+        return <WaitPattern>{
+            tiles: [tile, next2],
+            type: WaitPatternType.KANCHAN,
+        }
+    }
+
+    //only tanki
+    return <WaitPattern>{
+        tiles: [tile],
+        type: WaitPatternType.TANKI,
+    }
+}
+
+function getPairsCount(hand: HandStructure, onlyUnique: boolean): number {
+    const {manTiles, pinTiles, souTiles, honorTiles} = hand
+
+    const manPairs = getPairs(manTiles, onlyUnique)
+    const pinPairs = getPairs(pinTiles, onlyUnique)
+    const souPairs = getPairs(souTiles, onlyUnique)
+    const honorPairs = getPairs(honorTiles, onlyUnique)
+
+    return manPairs.length + pinPairs.length + souPairs.length + honorPairs.length
+}
+
+function groupDuplicates(tiles: Tile[]): {tile: Tile, amount: number}[] {
+    return tiles.reduce<{tile: Tile, amount: number}[]>((acc, tile) => {
+        const element = acc.find(x => x.tile.type === tile.type && x.tile.value === tile.value)
+        if (element !== undefined) {
+            const index = acc.indexOf(element)
+            acc[index].amount = acc[index].amount + 1
+        } else {
+            acc.push({
+                tile,
+                amount: 1,
+            })
+        }
+
+        return acc
+    }, [])
+}
+
+function getPairs(suitTiles: number[], onlyUnique: boolean): number[] {
+    const unique = suitTiles.filter((x, i, a) => a.indexOf(x) == i)
+    const pairs: number[] = []
+    for (let tile of unique) {
+        if (!onlyUnique && hasDuplicateTiles(suitTiles, tile, 4)) {
+            pairs.push(tile)
+            pairs.push(tile)
+        } else if (hasDuplicateTiles(suitTiles, tile, 2)) {
+            pairs.push(tile)
+        }
+    }
+
+    return pairs
+}
+
+function hasUniqueTiles(suitTiles: number[], ...tiles: number[]): boolean {
+    return tiles.every(tile => suitTiles.includes(tile))
+}
+
+function hasDuplicateTiles(suitTiles: number[], tile: number, amount: number): boolean {
+    return suitTiles.filter(x => x === tile).length >= amount
+}
